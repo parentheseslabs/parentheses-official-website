49dbfc8e5eaadd4df54f2f437c872fd6daf1ef19
Sayan67 <sayan.das.21@aot.edu.in>
2024-05-25 00:08:48 +0530

    Added local fonts and colors in tailwid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        'primary_blue':'#2876FF',
+        'text_black':'#161616',
+        'bg_gray':'#DFDFDF'
+      },
+      fontFamily:{
+        'sfpd-regular':['sfpd-regular','system-ui','sans-serif'],
+        'sfpd-semibold':['sfpd-semibold','system-ui','sans-serif'],
+        'sfpd-bold':['sfpd-bold','system-ui','sans-serif'],
+      }
     },
   },
   plugins: [],
