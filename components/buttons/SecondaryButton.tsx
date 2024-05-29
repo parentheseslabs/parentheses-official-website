ad324999e49ecb0305acd30e615fe37b7964454a
Sayan67 <sayan.das.21@aot.edu.in>
2024-05-29 09:59:25 +0530

    Add button click animation

--- components/buttons/SecondaryButton.tsx
+++ components/buttons/SecondaryButton.tsx
@@ -10,7 +10,7 @@ interface props
 const SecondaryButton = ({...props}:props) => 
 {
   return (
-    <button className='pl-8 pr-10 h-fit py-3 rounded-full border border-primary_blue hover:border-white  text-primary_blue hover relative group overflow-hidden flex items-center gap-2 hover:text-white duration-500 text-xs md:text-sm xl:text-[16px] z-50 bg-white'>
+    <button className='pl-8 pr-10 h-fit py-3 rounded-full border border-primary_blue hover:border-white  text-primary_blue hover relative group overflow-hidden flex items-center gap-2 hover:text-white duration-500 text-xs md:text-sm xl:text-[16px] z-50 bg-white active:scale-[.8]'>
         {props.title}
         <div className='bg-primary_blue rounded-full p-[0.18rem] absolute -z-10 right-5 group-hover:scale-[50] duration-[400ms]'></div>
         <DropDownArrow className='group-hover:stroke-white absolute -z-10 right-3 -rotate-90 stroke-2'/>
