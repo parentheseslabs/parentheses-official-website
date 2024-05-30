147485c2b8ed70af08afab3c176e65f7969dbbbf
Sayan67 <sayan.das.21@aot.edu.in>
2024-05-30 17:28:08 +0530

    fixed image src

--- components/aboutpage/OurValues.tsx
+++ components/aboutpage/OurValues.tsx
@@ -21,7 +21,7 @@ const OurValues = () => {
     return (
         <div className='flex justify-center items-center py-28 min-h-screen'>
             <div className='bg-primary_blue w-[93%]  rounded-2xl lg:rounded-[2rem] flex justify-center items-center relative text-white flex-col gap-14 lg:gap-20 py-28 px-20 text-center'>
-                <Image src={'/background/cardGrid.svg'} width={200} height={200} alt='grid' className='w-[60%] absolute -z-0 top-[50%] -translate-y-[50%] hidden md:flex opacity-50'></Image>
+                <Image src={'/background/cardGrid.png'} width={200} height={200} alt='grid' className='w-[60%] absolute -z-0 top-[50%] -translate-y-[50%] hidden md:flex opacity-50'></Image>
                 <h1 className='font-sfpd-bold text-3xl sm:text-4xl lg:text-5xl translate-y-4'>Our Values</h1>
                 <h2 className=' text-base sm:text-lg lg:text-2xl'>Our Core “Feed On” Prinicples</h2>
                 <div className='flex gap-14 lg:gap-32 sm:flex-row flex-col'>
