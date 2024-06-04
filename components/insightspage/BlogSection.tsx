78e71d9800e6a71a6ed823df0000dc7f4c615db7
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-04 20:52:58 +0530

    Add Blogs section in Insights

---
+++ components/insightspage/BlogSection.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import InsightCard from '@/components/insightspage/InsightCard'
+
+
+const data = [
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+    {
+        img: "/raster/blog1.png",
+        type: "News",
+        title: "Create a bigger funnel",
+        desc: "Insights, Trends and Expertise: Dive into our knowledge HubInsights, Trends and Expertise: Dive into our knowledge Hub.",
+        date: "12th May 2024",
+        author: "Parentheses Labs"
+
+    },
+
+]
+
+const BlogSection = () => {
+    return (
+        <div className='flex gap-16 justify-center items-center relative min-h-screen px-[7%] flex-wrap'>
+            <div className='flex min-w-[90%] justify-center gap-8'>
+                <button className='py-1 px-4 bg-blue-100 text-primary_blue  w-fit rounded-full sm:text-base md:text-lg lg:text-xl text-sm'>Blog</button>
+                <button className='py-1 px-4 bg-gray-300  w-fit rounded-full sm:text-base md:text-lg lg:text-xl text-sm'>Guides</button>
+            </div>
+                {
+                    data.map((blog, idx) => (
+                        <InsightCard key={idx} {...blog} />
+                    ))
+                }
+
+            <div className='w-[200vw] bg-gray-100 h-[30rem] rotate-12 absolute -z-[1] -left-[20%] sm:-left-[10%]'></div>
+        </div>
+    )
+}
+
+export default BlogSection
