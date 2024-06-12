700446882926bb008cd50c73cbb75eb216184ab8
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-12 17:32:37 +0530

    Add Hero section to service page

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import ServiceHero from '@/components/servicespage/ServiceHero'
+import ConversationCard from '@/components/servicespage/ConversationCard'
+
+const page = () => {
+  return (
+    <main className=''>
+        <ServiceHero/>
+    </main>
+  )
+}
+
+export default page
