7da6050884b1cbaea43adbc53fb1c547f2c4cb45
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-12 17:29:13 +0530

    Add functionality to primary button

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -8,10 +8,11 @@ interface props
     className?:string
 }
 
+
 const PrimaryButton = ({...props}:props) => 
 {
   return (
-    <button className={`${props.className} pl-8 pr-12 h-fit py-3 rounded-full border border-primary_blue  text-white hover relative group overflow-hidden flex items-center gap-2 hover:text-primary_blue duration-500 text-xs md:text-sm xl:text-[16px] z-0 bg-primary_blue active:scale-[.8]`}>
+    <button onClick={props.handleClick} className={`${props.className} pl-8 pr-12 h-fit py-3 rounded-full border border-primary_blue  text-white hover relative group overflow-hidden flex items-center gap-2 hover:text-primary_blue duration-500 text-xs md:text-sm xl:text-[16px] z-0 bg-primary_blue active:scale-[.8]`}>
         {props.title}
         <div className='bg-white rounded-full p-[0.18rem] absolute -z-10 right-5 group-hover:scale-[70] duration-[400ms]'></div>
         <DropDownArrow className=' group-hover:stroke-primary_blue absolute -z-10 right-3 -rotate-90 stroke-2'/>
