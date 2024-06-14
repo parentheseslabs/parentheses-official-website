fe98d41982d81314bc9ca33f244fd9318560fa36
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-14 15:29:01 +0530

    Add Service card

---
+++ components/servicespage/ServiceCard.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface cardProp {
+    props:{
+        title : string,
+        description : string  
+    },
+    Color:{
+        backgroundColor:string
+    },
+    images:{
+        img:string
+    }
+}
+
+const ServiceCard = ({props,Color,images}:cardProp) => {
+    let bgText ="";
+    
+
+  return (
+    <div className={`${Color.backgroundColor=="blue"?"bg-primary_blue":"bg-card_dark_blue"} w-60 h-72 text-white px-5 py-4 rounded-lg relative overflow-hidden`}>
+        <h1 className='text-lg md:text-2xl font-sfpd-semibold'>{props.title}</h1>
+        <h1 className='text-sm sm:text-base '>{props.description}</h1>
+        <Image src={images.img} width={200} height={200} alt='' className='absolute -right-10 -bottom-10 opacity-35'></Image>
+    </div>
+  )
+}
+
+export default ServiceCard
