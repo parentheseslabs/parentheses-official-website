8f33e57474d58c7735287f0ebbbac82f64d350ea
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-14 13:40:56 +0530

    Fix flex bug in insights blog section

--- components/insightspage/BlogSection.tsx
+++ components/insightspage/BlogSection.tsx
@@ -10,16 +10,19 @@ const BlogSection = async ({ uid }: { uid?: string }) => {
     const allData = blogs.filter((item) => item.data.slices[0]?.primary.isfeatured);
 
     return (
-        <div className='flex gap-16 justify-center items-center relative min-h-screen px-[7%] flex-wrap py-32'>
+        <div className='flex flex-col gap-16 justify-center items-center relative min-h-screen px-[7%] flex-wrap py-32'>
             <div className='flex min-w-[90%] justify-center gap-8'>
                 <button className='py-1 px-4 bg-blue-100 text-primary_blue  w-fit rounded-full sm:text-base md:text-lg lg:text-xl text-sm'>Blog</button>
                 <button className='py-1 px-4 bg-gray-300  w-fit rounded-full sm:text-base md:text-lg lg:text-xl text-sm'>Guides</button>
             </div>
-            {
-                allData.map((blog, idx) => (
-                    <InsightCard key={blog.uid} {...blog} />
-                ))
-            }
+            <div className='flex gap-16 flex-wrap justify-center'>
+
+                {
+                    allData.map((blog, idx) => (
+                        <InsightCard key={blog.uid} {...blog} />
+                    ))
+                }
+            </div>
             <Link href={'insights/blogs'}>
                 <PrimaryButton title='All Blogs' />
             </Link>
