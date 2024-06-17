083239135e431e92f32e0ba4131f17c2ad603efa
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-17 16:11:37 +0530

    Add new footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,6 +6,7 @@ import Phone from '@/public/svgs/phone.svg'
 import Mail from '@/public/svgs/mail.svg'
 import Linkedin from '@/public/svgs/linkedinWhite.svg'
 import data from '@/public/navlinks/data'
+import Link from 'next/link'
 
 
 const Footer: React.FC = () => {
@@ -13,30 +14,20 @@ const Footer: React.FC = () => {
   const linksHaveChildren = data.filter((item) => (item.haveChildren));
   const onlyLinks = data.filter((item) => (!item.haveChildren))
   return (
-    <div className='min-w-full min-h-[80svh] bg-primary_blue relative overflow-hidden py-36 md:py-56 px-[4%] text-white flex flex-col items-center'>
-      <div className='w-full space-y-6 flex flex-col gap-16'>
-        <div className='space-y-6 px-2'>
-          <FooterLogo />
-          <div className='flex md:items-center items-start gap-4 flex-col md:flex-row'>
-            <div className='flex items-center gap-1'>
-              <Phone  className='fill-white stroke-white'/>
-              <h1>+91 00000 00000</h1>
-            </div>
-            <a href='/' className='flex items-center gap-1'>
-              <Mail />
-              <h1>help@parentheses.com</h1>
-            </a>
-            <a href='/' className='flex items-center gap-1'>
-              <Linkedin />
-              <h1>Parentheses Labs</h1>
-            </a>
-          </div>
+    <div className='min-w-full min-h-[70svh] bg-primary_blue relative overflow-hidden pt-20 md:pt-48 pb-20 px-[10%] text-white flex flex-col items-center'>
+      <div className='w-full flex lg:flex-row flex-col justify-center gap-10 md:gap-20'>
+        <div className=' w-[15rem] lg:w-1/5 flex flex-col justify-between gap-4'>
+          <Link href={"/"}>
+            <FooterLogo className='w-[10rem]' />
+          </Link>
+          <div className='sm:text-base text-sm mb-12'>©Parentheses Labs, 2024.</div>
         </div>
-        <div className='flex border-dashed border-x border-white/40 md:border-none flex-wrap px-5 md:px-0 justify-between'>
+        <div className='flex flex-wrap justify-between w-full '>
+
           {
             linksHaveChildren.map((item, idx) => (
-              <div key={idx} className=' flex-col flex gap-4 md:border-dashed md:first:border-l md:border-r md:border-white/40 md:px-8 w-[9rem] md:w-1/4 mt-16'>
-                <h1 className='text-lg sm:text-xl font-sfpd-bold'>{item.title}</h1>
+              <div key={idx} className=' flex-col flex gap-4 md:gap-5 md:px-8 w-[9rem] md:w-[12rem] mb-12'>
+                <h1 className='text-base md:text-lg font-sfpd-semibold'>{item.title}</h1>
                 {
                   item.children?.map((child, key) => (
                     <a href={child.href} key={key} className='sm:text-base text-sm'>{child.subtitle}</a>
@@ -45,18 +36,17 @@ const Footer: React.FC = () => {
               </div>
             ))
           }
-          <div className=' border-dashed md:border-r  md:border-white/40 md:px-8 w-[9.5rem] md:w-1/4 flex flex-col gap-6 mt-16'>
-
+          <div className=' md:px-8 w-[9rem] lg:mt-0 md:w-[15rem] flex flex-col gap-6 '>
+            <h1 className='text-base md:text-lg font-sfpd-semibold'>More</h1>
             {
               onlyLinks.map((item, key) => (
-                <a key={key} href={item.href} className='text-xl font-sfpd-bold'>{item.title}</a>
+                <a key={key} href={item.href} className='sm:text-base text-sm'>{item.title}</a>
               ))
             }
           </div>
         </div>
       </div>
-      <div className='absolute bottom-0 border-dashed border-white/40 border-t py-2 w-full flex justify-center'>©Parentheses Labs, 2024. All rights reserved.</div>
-      <BlockImageDark className=' absolute left-0 top-0 w-[60%] md:-left-40  md:-top-[30%] opacity-70' />
+      <BlockImageDark className=' absolute left-0 top-0 w-[45%] md:-left-40  md:-top-[30%] opacity-40' />
     </div>
   )
 }
