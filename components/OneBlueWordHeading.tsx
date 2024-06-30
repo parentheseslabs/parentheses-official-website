df5d5182e0aa48bf57fbd700d7f6d9eaa80ad040
Sayan67 <sayan.das.21@aot.edu.in>
2024-06-30 20:31:37 +0530

    Update one blue word heading

--- components/OneBlueWordHeading.tsx
+++ components/OneBlueWordHeading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OneBlueWordHeading = ({ heading,wordNum,className }: { heading: string,wordNum:number,className?:string }) => {
     const words = heading.split(" ");
     return (
-        <div className={`${className} flex gap-2`}>
+        <div className={`${className} flex gap-2 flex-wrap`}>
             {
                 words.map((word,idx)=>(
                     <h1 key={idx} className={`${idx+1===wordNum && "text-primary_blue"} font-sfpd-bold text-[3.4vmax]`}>{word}</h1>
