4ec202849db3659c60a95a1ccad666904b4c4f31
Sayan67 <sayan.das.21@aot.edu.in>
2024-07-10 11:24:41 +0530

    Subservice page carousel change

--- components/subservice/CaseStudiesCarousel.tsx
+++ components/subservice/CaseStudiesCarousel.tsx
@@ -1,8 +1,9 @@
 "use client"
 import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import CarouselCard from './CarouselCard';
@@ -38,25 +39,35 @@ const CaseStudiesCarousel = () => {
     <section className=' min-h-svh space-y-4 m'>
       <h1 className='text-center text-primary_blue font-sfpd-semibold text-base md:text-lg'>CASE STUDIES</h1>
       <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        slidesPerView={'auto'}
         allowTouchMove={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 10,
+          depth: 100,
+          modifier: 0.8,
+          slideShadows: true,
+        }}
         autoplay={{
           delay: 4000,
           disableOnInteraction: false,
         }}
-        loop
+        // loop
         pagination={{
           clickable: true
         }}
         navigation
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation, EffectCoverflow]}
         centeredSlides={true}
-        spaceBetween={10}
+        spaceBetween={0}
         className=' bg-gray-100 flex justify-center'>
         {
           caseStudies.map((item, idx) => (
 
             <SwiperSlide key={idx} className='py-20'>
-              <CarouselCard {...item}/>
+              <CarouselCard {...item} />
             </SwiperSlide>
           ))
 
